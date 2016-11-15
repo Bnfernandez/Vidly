7349f8f6f311cd7a7ccb91642ed8299f1a161bbd
basto <basto@DESKTOP-K496AO3>
2016-11-15 13:29:53 +0100

    Added Customers and Movies functionalities.

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        List<Customer> customers = new List<Customer>()
+            {
+                new Customer() { Name = "John Williams" , Id = 1},
+                new Customer() { Name = "Alain Dupont" , Id = 2}
+            };
+
+
+        // GET: Customers
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //[Route("movies/released/{year:regex(\\d{4}):range(2014, 2019)}/{month:regex(\\d{2}):range(1, 12)}")]
+        [Route("customers/summary")]
+        public ActionResult CustomersSummary()
+        {
+            var viewModel = new CustomersSummaryViewModel
+            {
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+
+        [Route("customers/details/{id}")]
+        public ActionResult CustomerDetail(int id)
+        {
+            Customer customerTemp = null;
+
+            foreach (var customer in customers)
+            {
+                if (customer.Id == id)
+                    customerTemp = customer;
+            }
+            var viewModel = new CustomerDetailViewModel() {Customer = customerTemp};
+
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+
+        [Route("Movies/")]
+        public ActionResult MoviesSummary()
+        {
+            var movies = new List<Movie>()
+            {
+                new Movie() {Name = "Shrek"},
+                new Movie() {Name = "Inception"}
+            };
+
+            var viewModel = new MoviesSummaryViewModel() {Movies = movies};
+
+            return View(viewModel);
+        }
+        
+        // GET: Movies
+/*        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+            var customers = new List<Customer>()
+            {
+                new Customer() { Name = "Customer 1" },
+                new Customer() { Name = "Customer 2" }
+            };
+
+            var viewModel = new RandomMovieViewModel()
+            {
+                Customers = customers,
+                Movie = movie
+            };
+            
+            return View(viewModel);
+        }*/
+
+        //mvcaction4 = quick way to create an action
+        [Route("movies/released/{year:regex(\\d{4}):range(2014, 2019)}/{month:regex(\\d{2}):range(1, 12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+
+    }
+}
